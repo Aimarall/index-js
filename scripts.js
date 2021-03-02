bfe83b95d40a3fac0d83929bf60ec3f9c1ef4d58
Aimarall <aimaral21072004@gmail.com>
2021-03-02 14:43:13 +0600

    added let to function

--- scripts.js
+++ scripts.js
@@ -9,3 +9,10 @@ document.querySelector('#example-2 button').addEventListener('click', function (
     counter++;
     document.querySelector('#example-2 h1').innerText = counter;
 });
+
+
+let name = "";
+document.querySelector('#example-3 input').addEventListener('input', function () {
+    name = this.value.trim();
+    document.querySelector('#example-3 h1').innerText = `Your name is "${name}"`;
+});
