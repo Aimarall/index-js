8df9c4ebef6757b0b1edc9f53193a43cee4a7da3
Aimarall <aimaral21072004@gmail.com>
2021-03-02 14:55:45 +0600

    added let inputs to js

--- scripts.js
+++ scripts.js
@@ -16,3 +16,29 @@ document.querySelector('#example-3 input').addEventListener('input', function ()
     name = this.value.trim();
     document.querySelector('#example-3 h1').innerText = `Your name is "${name}"`;
 });
+
+
+let text = "";
+let text1 = document.querySelector('#text-1');
+let text2 = document.querySelector('#text-2');
+let text3 = document.querySelector('#text-3');
+
+text1.addEventListener('input', function() {
+    text = this.value;
+    text1.value = text;
+    text2.value = text;
+    text3.value = text;
+});
+text2.addEventListener('input', function() {
+    text = this.value;
+    text1.value = text;
+    text2.value = text;
+    text3.value = text;
+})
+text3.addEventListener('input', function() {
+    text = this.value;
+    text1.value = text;
+    text2.value = text;
+    text3.value = text;
+})
+
